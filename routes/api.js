46611a204cf92f417dc23a1c7961e61608e842ab
luckyume <jonathanluckyyap@gmail.com>
2026-01-08 10:44:43 +0700

    Finalize kali

--- routes/api.js
+++ routes/api.js
@@ -12,24 +12,24 @@ module.exports = function (app) {
     const unit = convertHandler.getUnit(input);
 
     if (num === 'invalid number' && unit === 'invalid unit') {
-      return res.send('invalid number and unit');
+      return res.type('text').send('invalid number and unit');
     }
     if (num === 'invalid number') {
-      return res.send('invalid number');
+      return res.type('text').send('invalid number');
     }
     if (unit === 'invalid unit') {
-      return res.send('invalid unit');
+      return res.type('text').send('invalid unit');
     }
 
     const returnUnit = convertHandler.getReturnUnit(unit);
-    const convertedNum = convertHandler.convert(num, unit);
+    const returnNum = convertHandler.convert(num, unit);
 
     res.json({
       initNum: num,
       initUnit: unit,
-      returnNum: convertedNum,
-      returnUnit: returnUnit,
-      string: `${num} ${convertHandler.spellOutUnit(unit)} converts to ${convertedNum} ${convertHandler.spellOutUnit(returnUnit)}`
+      returnNum,
+      returnUnit,
+      string: `${num} ${convertHandler.spellOutUnit(unit)} converts to ${returnNum} ${convertHandler.spellOutUnit(returnUnit)}`
     });
   });
 };
